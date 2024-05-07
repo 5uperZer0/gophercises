f0fdcbf4c530a23db29a5271ce76e6187d6cc28a
5uperzer0 <natedagrape2@gmail.com>
2024-05-07 14:22:26 -0400

    Broke into functions with some AI assistance

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -10,73 +10,97 @@ import (
 	"time"
 )
 
+type problem struct {
+	q string
+	a string
+}
+
 func main() {
 
 	// Process flags
 	var shuffle bool
 	flag.BoolVar(&shuffle, "r", false, "Randomize questions")
-	timeLimit := flag.Int("limit", 3, "the time limit for the quiz in seconds")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	csvFilename := flag.String("csv", "problems.csv", "A csv file in the format of 'question, answer'")
 	flag.Parse()
 
-	file, err := os.Open(*csvFilename)
+	// Import CSV data and transform to problem struct
+	problems, err := formatCSV(*csvFilename)
 	if err != nil {
 		fmt.Printf("Failed to open the CSV file: %s", *csvFilename)
 		os.Exit(1)
 	}
-	defer file.Close()
 
-	// CSV Reader
-	reader := csv.NewReader(file)
-
-	//Enumerate CSV records
-	csv_records, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
-	}
-
-	// Shuffle questions if necessary
+	// Shuffle questions
 	if shuffle {
 		rand.New(rand.NewSource(time.Now().Unix()))
-		for i := len(csv_records) - 1; i > 0; i-- {
+		for i := len(problems) - 1; i > 0; i-- {
 			j := rand.Intn(i + 1)
-			csv_records[i], csv_records[j] = csv_records[j], csv_records[i]
+			problems[i], problems[j] = problems[j], problems[i]
 		}
 	}
 
-	// Instantiate quiz timer
-	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
-	defer timer.Stop()
-
-	// Run quiz loop!
-	round := 0
-	score := 0
-
-	for _, record := range csv_records {
-
-		fmt.Printf("Problem #%d: %s = ", round+1, record[0])
-		answer_ch := make(chan string)
-		go func() {
-			var in string
-			fmt.Scanln(&in)
-			in = strings.ReplaceAll(in, " ", "")
-			answer_ch <- in
-		}()
-
-		select {
-		case <-timer.C:
-			fmt.Printf("\nYou scored %d/12!\n", score)
-			return
-		case answer := <-answer_ch:
-			if answer == record[1] {
-				fmt.Println("Correct!")
-				score++
-			}
-		}
-	}
+	score := quiz(problems, *timeLimit)
 
 	// Print score and exit
-	fmt.Printf("\nYou scored %d/12!\n", score)
+	fmt.Printf("You scored %d/12\n", score)
 	os.Exit(0)
+}
+
+func formatCSV(filename string) ([]problem, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	csv_records, err := reader.ReadAll()
+	ret := make([]problem, len(csv_records))
+
+	for i, line := range csv_records {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+
+	return ret, err
+}
+
+func quiz(problems []problem, timeLimit int) int {
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+	defer timer.Stop()
+
+	score := 0
+	for i, problem := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, problem.q)
+		answer := getUserInput(timer)
+		if answer == problem.a {
+			fmt.Println("Correct!")
+			score++
+		} else if answer == "END" {
+			break
+		}
+	}
+	return score
+}
+
+func getUserInput(timer *time.Timer) string {
+	answer_ch := make(chan string)
+	go func() {
+		var in string
+		fmt.Scanln(&in)
+		in = strings.ReplaceAll(in, " ", "")
+		answer_ch <- in
+	}()
+
+	select {
+	case <-timer.C:
+		fmt.Println("\nTime's up!")
+		return "END"
+	case answer := <-answer_ch:
+		return answer
+	}
 }
